099c68829d803be4fb5f69d3606f71bf56fb143a
alloyking1 <alloyking1@gmail.com>
2020-05-06 06:07:27 -0400

    profile look fixed, looks better now

--- src/views/pages/dashBoardSponsor/SponsorProfile.vue
+++ src/views/pages/dashBoardSponsor/SponsorProfile.vue
@@ -2,8 +2,7 @@
     <div>
         <vs-tabs alignment="center">
             <vs-tab label="Profile">
-                <div class="vx-col w-full lg:w-1/3">
-                    <!-- ABOUT CARD -->
+                <!-- <div class="vx-col w-full lg:w-1/3">
                     <vx-card title="About" class="mt-base">
                         
                         <template slot="actions">
@@ -46,7 +45,52 @@
                         </div> 
 
                     </vx-card>
-                </div>
+                </div> -->
+
+                <div class="vx-col w-full lg:w-1/3 sm:w-1/2 mb-base">
+                        <vx-card class="p-2">
+                            <vs-avatar class="mx-auto mb-6 block" size="80px" src="https://www.bing.com/images/search?view=detailV2&ccid=ydgLdMPR&id=1DC342F26F911C03617CB94E81A96F78345125BA&thid=OIP.XZ0Y8eEx1e7nyYJIO_IfpgHaHa&mediaurl=http%3a%2f%2f4.bp.blogspot.com%2f-zsbDeAUd8aY%2fUS7F0ta5d9I%2fAAAAAAAAEKY%2fUL2AAhHj6J8%2fs1600%2ffacebook-default-no-profile-pic.jpg&exph=1290&expw=1290&q=Facebook+Avatar+Icon&simid=608049184228052981&selectedIndex=3" />
+                            <div class="text-center">
+                                <h4>{{ this.sponsorEdit.user.name }}</h4>
+                                <p class="text-grey">{{ this.sponsorEdit.user.email }}</p>
+                            </div>
+                            <div class="mb-4 mt-base">
+                                <div class="flex justify-between">
+                                    <small class="font-semibold">120 points</small>
+                                    <small class="text-grey">100</small>
+                                </div>
+                                <vs-progress percent="80" class="block mt-1 shadow-md"></vs-progress>
+                                profile completed
+                            </div>
+                            
+                            <div class="mb-4 mt-base">
+                                <div class="text-center">
+                                    <small>Profile Details</small>
+                                </div>
+                            </div>
+                            <div class="flex justify-between flex-wrap">
+                                <h6>Phone Number:</h6>
+                                <p>{{this.sponsorEdit.phone_number}}</p>
+                                <vs-divider />
+                                <h6>Address:</h6>
+                                <p>{{this.sponsorEdit.address || "Edit to add Address"}}</p>
+                                <vs-divider />
+                                <h6>City:</h6>
+                                <p>{{this.sponsorEdit.city || "Edit to add city"}}</p>
+                                <vs-divider />
+                                <h6>State:</h6>
+                                <p>{{this.sponsorEdit.state || "Edit to add state"}}</p>
+                                <vs-divider />
+                                <h6>Country:</h6>
+                                <p>{{this.sponsorEdit.country || "Edit to add country"}}</p>
+                                <vs-divider />
+                            </div>
+                            <!-- <div class="flex justify-between flex-wrap">
+                                <vs-button class="mt-4 mr-2 shadow-lg" type="gradient" color="#7367F0" gradient-color-secondary="#CE9FFC">{{ card_6.btn_left_text }}</vs-button>
+                                <vs-button class="mt-4" type="border" color="#b9b9b9">{{ card_6.btn_right_text }}</vs-button>
+                            </div> -->
+                        </vx-card>
+                    </div>
             </vs-tab>
 
             <vs-tab label="Edit">
