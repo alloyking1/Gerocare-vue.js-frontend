0c3e445392f6ab120f290027dac5edc0a9ddc8a3
alloyking1 <alloyking1@gmail.com>
2020-06-25 07:44:38 -0400

    complete re-structure of the subscription component and UI. services splited into different components and the entire UI flow changed

--- src/views/pages/dashBoardSponsor/Subscription.vue
+++ src/views/pages/dashBoardSponsor/Subscription.vue
@@ -4,36 +4,49 @@
         <div class="vx-col w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 mb-base">
 
             <vx-card class="mt-2 mb-2">
-                <h3 style="font-size:10px" class="pb-2">HOME VISITS</h3>
-                <vs-button radius color="danger" type="gradient" icon-pack="feather" icon="icon-user-plus"></vs-button>
-            </vx-card>
-            <vx-card class="mt-2 mb-2">
-                <h3 style="font-size:10px" class="pb-2">TELE-CONSULTATION</h3>
-                <vs-button radius color="danger" type="gradient" icon-pack="feather" icon="icon-user-plus"></vs-button>
-            </vx-card>
-            <vx-card class="mt-2 mb-2">
-                <h3 style="font-size:10px" class="pb-2">ON-DEMAN VISIT</h3>
-                <vs-button radius color="danger" type="gradient" icon-pack="feather" icon="icon-user-plus"></vs-button>
+                <h3 style="font-size:10px" class="pb-2">SELECT A SERVICE TO SUBSCRIBE TO</h3>
+                <vs-divider></vs-divider>
+                <p style="font-size:10px" class="pb-2">Home Visit Services</p>
+                <vs-button color="primary" type="filled" @click="popupActivo=true">SUBSCRIBE</vs-button>
+
+                <vs-divider></vs-divider>
+                <p style="font-size:10px" class="pb-2">Tele-Consultation Service</p>
+                <vs-button color="success" type="filled" @click="popupActivo=true">SUBSCRIBE</vs-button>
+                <vs-divider></vs-divider>
+                
+                <p style="font-size:10px" class="pb-2">On-demand Visitation Service</p>
+                <vs-button color="warning" type="filled" @click="popupActivo=true">SUBSCRIBE</vs-button>
             </vx-card>
             
         </div>
 
         <div class="vx-col w-full sm:w-1/2 md:w-1/2 lg:w-3/4 xl:w-3/4 mb-base">
-            <vx-card title="">
-                <div class="container w-full">
-                    <div>txt</div>
-                </div>
-            </vx-card>
+            <div class="container w-full">
+                <subscriptionHistory></subscriptionHistory>
+            </div>
+        </div>
+
+        <div>
+            <vs-popup :active.sync="popupActivo">
+                <subscriptionPayment></subscriptionPayment>
+            </vs-popup>
         </div>
     </div>
 </template>
 
 <script>
-    import sideBar from '../Components/subscriptionNav'
+    import subscriptionHistory from '../Components/subscriptionHistory'
+    import subscriptionPayment from '../Components/subscriptionPayment'
 
     export default{
+        data(){
+            return{
+                popupActivo:false
+            }
+        },
         components:{
-            sideBar
+            subscriptionHistory,
+            subscriptionPayment
         }
     }
 </script>
