5e3f10de9f2dc9d68aee6ff9bfbdb5634c13a825
alloyking1 <alloyking1@gmail.com>
2020-06-25 06:42:37 -0400

    sending changes to heroku

--- src/views/pages/Components/subscriptionNav.vue
+++ src/views/pages/Components/subscriptionNav.vue
@@ -1,33 +1,49 @@
-<template>
+<template lang="html">
 
-    <div id="parentx-demo-2">
+  <div ref="parentSidebar" id="parentx">
 
-        <vs-button @click="active=!active" color="primary" type="filled">Open Sidebar</vs-button>
-        <vs-sidebar parent="#parentx-demo-2" :hidden-background="true" default-index="1"  color="primary" class="sidebarx" spacer v-model="active">
+    <vs-button @click="active=!active" color="primary" type="filled">Open Sidebar</vs-button>
+    <vs-sidebar :parent="$refs.parentSidebar" default-index="1"  color="primary" class="sidebarx" spacer v-model="active">
 
-        <div class="header-sidebar" slot="header">
-            <h4>
-            My Name
-            <vs-button color="primary" icon="more_horiz" type="flat"></vs-button>
-            </h4>
+      <div class="header-sidebar" slot="header">
+        <vs-avatar  size="70px" src="https://randomuser.me/api/portraits/men/85.jpg"/>
 
-        </div>
+        <h4>
+          My Name
+          <vs-button color="primary" icon="more_horiz" type="flat"></vs-button>
+        </h4>
 
-        <vs-sidebar-item index="1" icon="question_answer"> Dashboard </vs-sidebar-item>
-        <vs-sidebar-item index="2" icon="gavel"> History </vs-sidebar-item>
+      </div>
 
-        <vs-divider icon="person" position="left"> User </vs-divider>
+      <vs-sidebar-item index="1" icon="question_answer">
+        Dashboard
+      </vs-sidebar-item>
 
-        <vs-sidebar-item index="3" icon="verified_user"> Configurations </vs-sidebar-item>
-        <vs-sidebar-item index="4" icon="account_box"> Profile </vs-sidebar-item>
-        <vs-sidebar-item index="5" > Card </vs-sidebar-item>
+      <vs-sidebar-item index="2" icon="gavel">
+        History
+      </vs-sidebar-item>
 
-        <div class="footer-sidebar" slot="footer">
-            <vs-button icon="reply" color="danger" type="flat">log out</vs-button>
-            <vs-button icon="settings" color="primary" type="border"></vs-button>
-        </div>
-        </vs-sidebar>
-    </div>
+      <vs-divider icon="person" position="left">
+        User
+      </vs-divider>
+
+      <vs-sidebar-item index="3" icon="verified_user">
+        Configurations
+      </vs-sidebar-item>
+      <vs-sidebar-item index="4" icon="account_box">
+        Profile
+      </vs-sidebar-item>
+      <vs-sidebar-item index="5" >
+        Card
+      </vs-sidebar-item>
+
+      <div class="footer-sidebar" slot="footer">
+        <vs-button icon="reply" color="danger" type="flat">log out</vs-button>
+        <vs-button icon="settings" color="primary" type="border"></vs-button>
+      </div>
+
+    </vs-sidebar>
+  </div>
 
 </template>
 
@@ -39,39 +55,32 @@ export default {
 }
 </script>
 
-<style lang="scss">
-#parentx {
-  overflow: hidden;
-  height: 500px;
-  position: relative;
-}
+<style lang="stylus">
+#parentx
+  overflow hidden
+  height 500px
+  position relative
 
-.header-sidebar {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  width: 100%;
-  h4 {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    > button {
-      margin-left: 10px;
-    }
-  }
-}
-
-.footer-sidebar {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-  > button {
-    border: 0px solid rgba(0, 0, 0, 0) !important;
-    border-left: 1px solid rgba(0, 0, 0, 0.07) !important;
-    border-radius: 0px !important;
-  }
-}
+.header-sidebar
+  display flex
+  align-items center
+  justify-content center
+  flex-direction column
+  width 100%
+  h4
+    display flex
+    align-items center
+    justify-content center
+    width 100%
+    > button
+      margin-left 10px
+.footer-sidebar
+  display flex
+  align-items center
+  justify-content space-between
+  width 100%
+  > button
+      border 0px solid rgba(0,0,0,0) !important
+      border-left 1px solid rgba(0,0,0,.07) !important
+      border-radius 0px !important
 </style>
--- src/views/pages/dashBoardSponsor/Subscription.vue
+++ src/views/pages/dashBoardSponsor/Subscription.vue
@@ -1,16 +1,43 @@
+
 <template>
-    <div class="container-fluid">
-        <sideBar class=""></sideBar>
+    <div class="vx-row">
+        <div class="vx-col w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 mb-base">
+
+            <vx-card class="mt-2 mb-2">
+                <h3 style="font-size:10px" class="pb-2">HOME VISITS</h3>
+                <vs-button radius color="danger" type="gradient" icon-pack="feather" icon="icon-user-plus"></vs-button>
+            </vx-card>
+            <vx-card class="mt-2 mb-2">
+                <h3 style="font-size:10px" class="pb-2">TELE-CONSULTATION</h3>
+                <vs-button radius color="danger" type="gradient" icon-pack="feather" icon="icon-user-plus"></vs-button>
+            </vx-card>
+            <vx-card class="mt-2 mb-2">
+                <h3 style="font-size:10px" class="pb-2">ON-DEMAN VISIT</h3>
+                <vs-button radius color="danger" type="gradient" icon-pack="feather" icon="icon-user-plus"></vs-button>
+            </vx-card>
+            
+        </div>
+
+        <div class="vx-col w-full sm:w-1/2 md:w-1/2 lg:w-3/4 xl:w-3/4 mb-base">
+            <vx-card title="">
+                <div class="container w-full">
+                    <div>txt</div>
+                </div>
+            </vx-card>
+        </div>
     </div>
 </template>
 
 <script>
-import sideBar from '../Components/subscriptionNav'
+    import sideBar from '../Components/subscriptionNav'
 
-export default{
-    components:{
-        sideBar
+    export default{
+        components:{
+            sideBar
+        }
     }
-}
 </script>
 
+<style scoped>
+
+</style>
